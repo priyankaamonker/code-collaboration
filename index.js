57c6f603748209ef4c0e1601fc81c2d4d4803e50
Priyanka Amonker <priyanka@Priyankas-MacBook-Pro.local>
2024-11-21 00:50:45 -0800

    adding files

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
+const cluster = require('node:cluster');
+const { createAdapter, setupPrimary } = require('@socket.io/cluster-adapter');
+
+if (cluster.isPrimary) {
+  // create two workers - port number being randomly generated
+  cluster.fork({
+      PORT: Math.floor(50000 + (25000 - 50000) * Math.random())
+  });
+  cluster.fork({
+      PORT: Math.floor(50000 + (25000 - 50000) * Math.random())
+  });
+
+  // set up the adapter on the primary thread
+  return setupPrimary();
+}
+
+async function main() {
+  const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database
+  });
+
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+    );
+  `);
+
+  await db.run('delete from messages'); // remove old content 
+
+  const app = express();
+  const server = createServer(app);
+  const io = new Server(server, {
+    connectionStateRecovery: {},
+    adapter: createAdapter()
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+  });
+
+  io.on('connection', async (socket) => {
+    socket.on('chat message', async (msg, clientOffset, callback) => {
+      let result;
+      try {
+        result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, clientOffset);
+      } catch (e) {
+        if (e.errno === 19 /* SQLITE_CONSTRAINT */ ) {
+          callback();
+        } else {
+          // nothing to do, just let the client retry
+        }
+        return;
+      }
+      io.emit('chat message', msg, result.lastID);
+      callback();
+    });
+
+    if (!socket.recovered) {
+      try {
+        await db.each('SELECT id, content FROM messages WHERE id > ?',
+          [socket.handshake.auth.serverOffset || 0],
+          (_err, row) => {
+            socket.emit('chat message', row.content, row.id);
+          }
+        )
+      } catch (e) {
+        // something went wrong
+      }
+    }
+  });
+
+  const port = process.env.PORT;
+
+  server.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
+  });
+}
+
+main();
---
+++ readme.md
@@ -0,0 +1,16 @@
+REF: https://socket.io/docs/v4/tutorial/introduction
+1: Install dependencies on host (nodev18.20.5 , express@4 (npm install express@4), sqlite3)
+2: cd to application dir:
+3: run: node index.js
+4: browse: 
+    server running at http://localhost:3002
+    server running at http://localhost:3004
+    server running at http://localhost:3000
+    server running at http://localhost:3003
+    server running at http://localhost:3001
+    server running at http://localhost:3005
+    server running at http://localhost:3007
+    server running at http://localhost:3009
+    server running at http://localhost:3008
+    server running at http://localhost:3006
+    server running at http://localhost:3010
